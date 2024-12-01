78681e82d6ca0e59ab30e00f829eae993ecf3ac1
Till Knollmann <till-knollmann@outlook.de>
2024-12-01 10:25:29 +0000

    add solution to day 1

--- Day 1/day-1.py
+++ Day 1/day-1.py
@@ -2,6 +2,7 @@ from datetime import date
 import numpy as np
 import time
 import pprint
+import re
 
 from importlib.machinery import SourceFileLoader
 
@@ -17,37 +18,56 @@ path = ""
 def parseInput(input):
     result = None
 
-    # Todo Input parsing
+    pattern = re.compile(r'\d+')
+
+    left_list = []
+    right_list = []
+
+    for line in input:
+        numbers = pattern.findall(line)
+        left_list.append(int(numbers[0]))
+        right_list.append(int(numbers[1]))
+
+    result = (left_list, right_list)
 
     return result
 
+def absolute_value(x):
+    return x if x >= 0 else -x
 
 def part1(data, measure=False):
     startTime = time.time()
-    result_1 = None
+    result_1 = 0
 
-    input = parseInput(data)
+    (left_list, right_list) = parseInput(data)
 
-    # Todo program part 1
+    left_list.sort()
+    right_list.sort()
+
+    for i in range(len(left_list)):
+        result_1 += absolute_value(right_list[i] - left_list[i])
 
     executionTime = round(time.time() - startTime, 2)
     if measure:
         print("\nPart 1 took: " + str(executionTime) + " s")
-    return result_1
+    return str(result_1)
 
 
 def part2(data, measure=False):
     startTime = time.time()
-    result_2 = None
+    result_2 = 0
 
-    input = parseInput(data)
+    (left_list, right_list) = parseInput(data)
 
-    # Todo program part 2
+    right_list_frequencies = dict((number, right_list.count(number)) for number in set(left_list))
+
+    for left_num in left_list:
+        result_2 += left_num * right_list_frequencies[left_num]
 
     executionTime = round(time.time() - startTime, 2)
     if measure:
         print("\nPart 2 took: " + str(executionTime) + " s")
-    return result_2
+    return str(result_2)
 
 
 def runTests(test_sol_1, test_sol_2, path):
@@ -63,7 +83,7 @@ def runTests(test_sol_1, test_sol_2, path):
 
     success_1 = [(test_sol_1[i] == test_res_1[i]) for i in range(len(test_sol_1))]
     success_2 = [
-        "Part 2 Test " + str(i + 1) + " " + (test_sol_2[i] == test_res_2[i])
+        "Part 2 Test " + str(i + 1) + " " + str(test_sol_2[i] == test_res_2[i])
         for i in range(len(test_sol_2))
     ]
 
@@ -102,19 +122,19 @@ def main():
     global path
     path = "Day " + str(day) + "/"
 
-    test_sol_1 = []  # Todo put in test solutions part 1
-    test_sol_2 = []  # Todo put in test solutions part 2
+    test_sol_1 = [ "11" ]
+    test_sol_2 = [ "31" ]
 
-    test = True  # Todo
+    test = True
 
-    sol1 = sub1 = False  # Todo
-    sol2 = sub2 = False  # Todo
+    sol1 = sub1 = False
+    sol2 = sub2 = True
 
     if test:
         if not runTests(test_sol_1, test_sol_2, path):
             sub1 = sub2 = False
 
-    data_main = get_data(day=day, year=2021).splitlines()
+    data_main = get_data(day=day, year=2024).splitlines()
 
     if sol1:
         result_1 = part1(data_main, True)
@@ -127,10 +147,10 @@ def main():
     print("\n")
 
     if sub1:
-        submit(int(result_1), part="a", day=day, year=2021)
+        submit(int(result_1), part="a", day=day, year=2024)
 
     if sub2:
-        submit(int(result_2), part="b", day=day, year=2021)
+        submit(int(result_2), part="b", day=day, year=2024)
 
 
 if __name__ == "__main__":
