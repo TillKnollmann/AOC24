b2f704edf6c5e1cf9c1475a5947d4c902f7f9279
Till Knollmann <till-knollmann@outlook.de>
2024-12-10 22:24:31 +0000

    add day 10 part 1

---
+++ day-10/day-10.py
@@ -0,0 +1,189 @@
+from datetime import date
+import numpy as np
+import time
+import pprint
+import math
+import re
+from copy import deepcopy
+
+from importlib.machinery import SourceFileLoader
+
+lib = SourceFileLoader("lib", "lib.py").load_module()
+
+from aocd import get_data
+from aocd import submit
+
+day = 10
+path = ""
+
+class Game:
+
+    def __init__(self, map: list[list[int]], size_x: int, size_y: int):
+        self.map = map
+        self.size_x = size_x
+        self.size_y = size_y
+
+def parseInput(input: list[str]) -> Game:
+
+    map = []
+
+    size_x = len(input[0])
+    size_y = len(input)
+
+    for x in range(len(input[0])):
+        map.append([int(input[y][x]) for y in range(len(input))])
+
+    return Game(map, size_x, size_y)
+
+def get_reachable_summits(game: Game, x: int, y: int, known_paths: list[list[set[tuple[int,int]]]]) -> set[tuple[int,int]]:
+
+    if game.map[x][y] == 9:
+        return {(x,y)}
+
+    neighbors = [
+        (x-1, y),
+        (x+1, y),
+        (x, y-1),
+        (x, y+1)
+    ]
+
+    union = set()
+
+    for (neighbor_x, neightbor_y) in neighbors:
+        if is_inside(game, neighbor_x, neightbor_y):
+            if game.map[x][y] == game.map[neighbor_x][neightbor_y] - 1:
+                for summit in known_paths[neighbor_x][neightbor_y]:
+                    union.add(summit)
+
+    return union
+
+def is_inside(game: Game, x: int, y: int) -> bool:
+
+    return x >= 0 and x < game.size_x and y >= 0 and y < game.size_y
+
+
+def part1(data, measure=False):
+
+    startTime = time.time()
+    result_1 = 0
+
+    game = parseInput(data)
+
+    known_paths = [[ None for y in range(game.size_y) ] for x in range(game.size_x)]
+
+    for height in reversed(range(10)):
+        for x in range(game.size_x):
+            for y in range(game.size_y):
+                if game.map[x][y] == height:
+                    known_paths[x][y] = get_reachable_summits(game, x, y, known_paths)
+
+    for x in range(game.size_x):
+        for y in range(game.size_y):
+            if game.map[x][y] == 0:
+                result_1 += len(known_paths[x][y])
+
+    executionTime = round(time.time() - startTime, 2)
+    if measure:
+        print("\nPart 1 took: " + str(executionTime) + " s")
+    return str(result_1)
+
+
+def part2(data, measure=False):
+
+    startTime = time.time()
+    result_2 = None
+
+    input = parseInput(data)
+
+    # Todo program part 2
+
+    executionTime = round(time.time() - startTime, 2)
+    if measure:
+        print("\nPart 2 took: " + str(executionTime) + " s")
+    return result_2
+
+
+def runTests(test_sol_1, test_sol_2, path):
+
+    test_res_1 = []
+    test_res_2 = []
+
+    all_check = True
+
+    paths = lib.getTestPaths(path)
+
+    test_res_1 += list(map(part1, map(lib.getDataLines, paths)))
+    test_res_2 += list(map(part2, map(lib.getDataLines, paths)))
+
+    success_1 = [(test_sol_1[i] == test_res_1[i]) for i in range(len(test_sol_1))]
+    success_2 = [(test_sol_2[i] == test_res_2[i]) for i in range(len(test_sol_2))]
+
+    for i in range(len(test_sol_1)):
+        if success_1[i]:
+            print("Part 1 Test " + str(i + 1) + " Succeeded!")
+        else:
+            print(
+                "Part 1 Test "
+                + str(i + 1)
+                + " Failed! Expected "
+                + str(test_sol_1[i])
+                + " received "
+                + test_res_1[i]
+            )
+            all_check = False
+
+    for i in range(len(test_sol_2)):
+        if success_2[i]:
+            print("Part 2 Test " + str(i + 1) + " Succeeded!")
+        else:
+            print(
+                "Part 2 Test "
+                + str(i + 1)
+                + " Failed! Expected "
+                + str(test_sol_2[i])
+                + " received "
+                + test_res_2[i]
+            )
+            all_check = False
+
+    return all_check
+
+
+def main():
+    global path
+    path = "day-" + str(day) + "/"
+
+    test_sol_1 = [ "36" ]
+    test_sol_2 = []  # Todo put in test solutions part 2
+
+    test = True
+
+    sol1 = sub1 = True
+    sol2 = sub2 = False  # Todo
+
+    if test:
+        if not runTests(test_sol_1, test_sol_2, path):
+            sub1 = sub2 = False
+
+    data_main = get_data(day=day, year=2024).splitlines()
+
+    if sol1:
+        result_1 = part1(data_main, True)
+        print("Result Part 1: " + str(result_1))
+
+    if sol2:
+        result_2 = part2(data_main, True)
+        print("Result Part 2: " + str(result_2))
+
+    print("\n")
+
+    if sub1:
+        submit(int(result_1), part="a", day=day, year=2024)
+
+    if sub2:
+        submit(int(result_2), part="b", day=day, year=2024)
+
+
+if __name__ == "__main__":
+    main()
+
